6db0fb5fd9ec8bc99950d2c88870c2db41e3067a
jihob28th <60122546+jihob28th@users.noreply.github.com>
2020-02-20 11:42:13 +0800

    Create show_categories.php

---
+++ show_categories.php
@@ -0,0 +1,26 @@
+<?php
+$json = file_get_contents("http://rdapi.herokuapp.com/product/read.php");
+$data = json_decode($json,true);
+$list = $data['records'];
+
+
+?>
+
+<h1> Product Category </h1>
+
+<table border="1px">
+    <tr>
+        <th>Category ID</th>
+        <th>Category Name</th>
+    </tr>
+<?php
+foreach($list as $value){
+    ?>
+    <tr>
+        <td><?php echo $value['category_id'];?></td>
+        <td><?php echo $value['category_name'];?></td>
+    </tr>
+<?php
+}
+?>
+</table>
