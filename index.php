3d8d07ab07f0b65272c780009b0c7da311500885
jihob28th <60122546+jihob28th@users.noreply.github.com>
2020-02-20 11:32:20 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php 
+$loadnav = (isset($_GET['loadnav']) && $_GET['loadnav'] != '') ? $_GET['loadnav']: '';
+?>
+<html> 
+    <head>
+      <title>  API Exercise 1 </title>
+    </head>
+    <body>
+                        <ul> 
+                        <li> <a href="index.php?loadnav=readprod"> Show Product</a> </li>
+                        <li> <a href="index.php?loadnav=readcat"> Show Categories </a> </li>
+                        <li> <a href="index.php?loadnav=create"> Create  </a> </li>
+                        <li> <a href="index.php?loadnav=delete"> Delete  </a> </li>
+                        </ul>
+               
+                        <?php 
+                        switch($loadnav){
+                            case 'readprod':
+                            require_once 'show.php';
+                            break;
+                            case 'readcateg':
+                            require_once 'show_categories.php';
+                            break;
+                            case 'create':
+                            require_once 'form_create.php';
+                            break;
+                            case 'delete':
+                            require_once 'form_delete.php';
+                            break;
+                        }
+                        ?>
+
+    </body>
+</html>
