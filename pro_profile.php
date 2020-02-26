4225f5690d6e8cd40d77e50b80c63248e825c702
jihob28th <60122546+jihob28th@users.noreply.github.com>
2020-02-27 07:11:17 +0800

    Update and rename show_categories.php to pro_profile.php

---
+++ pro_profile.php
@@ -0,0 +1,21 @@
+<?php
+ $id = $_GET['id'];
+ $json = file_get_contents('http://rdapi.herokuapp.com/product/read_one.php?id='.$id);
+ $data = json_decode($json,true);
+ $details = array('records' => $data);
+ $result = $details['records'];
+?>
+<html>
+			<h1><b><?php echo $result['name']; ?></b></h1>
+<table border=1>
+		<p> <b>Price:  </b><?php echo $result['price']; ?></p>
+		<p>	<b>Description:  </b><?php echo $result['description']; ?> </p>
+		<p>	<b>Category:  </b><?php echo $result['category_name'];?> </p>
+</table>
+		<footer>
+			<a href="index.php?page=update&id=<?php echo $id ?>">Update</a>
+			<a href="pro_delete.php?id=<?php echo $id ?>">Delete</a>
+		</footer>
+	</div>
+  </div>
+</html>
