f81cc23cf17b5e51c62f78401ab9ad21735d994b
jihob28th <60122546+jihob28th@users.noreply.github.com>
2020-02-27 06:55:58 +0800

    Update show.php

--- show.php
+++ show.php
@@ -1,7 +1,18 @@
 <?php
 $json = file_get_contents("http://rdapi.herokuapp.com/product/read.php");
 $data = json_decode($json,true);
-$list = $data['records'];
+
+$search = $_POST['search'];
+ 
+ if(isset($search)){
+	$jsearch = file_get_contents('http://rdapi.herokuapp.com/product/search.php?s='.$search);
+	$res = json_decode($jsearch,true);
+
+	$list = $res['records'];
+	
+ }else{
+	$list = $data['records'];
+ }
 
 
 ?>
@@ -12,7 +23,8 @@ $list = $data['records'];
     <tr>
         <td>Product</td>
         <td>Price</td>
-	<td>Category</td> 
+	<td>Category</td>
+	<th>Description</th>
     </tr>
 <?php
 foreach($list as $value){
@@ -21,6 +33,7 @@ foreach($list as $value){
         <td><a href="product_details.php?id=<?php echo $value['id'];?>"><?php echo $value['name'];?></a></td>
         <td><?php echo $value['price'];?></td>
 	<td><?php echo $value['category'];?></td>
+	<td><?php echo $value['description'];?></td>    
     </tr>
 <?php
 }
