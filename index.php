dfd0be3a36ed29c497e8b93ca91a740f316c52dd
jihob28th <60122546+jihob28th@users.noreply.github.com>
2020-03-02 21:12:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $loadnav = (isset($_GET['loadnav']) && $_GET['loadnav'] != '') ? $_GET['loadnav'
     <body>
                         <ul> 
                         <li> <a href="index.php?loadnav=readprod"> Show Product</a> </li>
+                        <li> <a href="index.php?loadnav=categories"> Categories</a> </li>
                         <li> <a href="index.php?loadnav=create"> Create  </a> </li>
                         </ul>
                
@@ -15,6 +16,9 @@ $loadnav = (isset($_GET['loadnav']) && $_GET['loadnav'] != '') ? $_GET['loadnav'
                         switch($loadnav){
                             case 'readprod':
                             require_once 'show.php';
+                            break;
+                            case 'categories':
+                            require_once 'categories.php';
                             break;
                             case 'create':
                             require_once 'form_create.php';
